bfbea51bf32eea54b490d07efe3ae5a8c73dc212
Rafael Barbosa <rafaeldacostabarbosa@gmail.com>
2021-11-12 19:07:01 -0300

    resolvendo bo das imagens

--- frontend/src/components/Dashboard/index.tsx
+++ frontend/src/components/Dashboard/index.tsx
@@ -1,51 +1,20 @@
-import { useEffect, useState } from "react";
-
-import { Product } from "../../types";
-import { formatPrice } from "../../util/format";
 import { ProductHeader } from "../ProductHeader";
 import { useProducts } from "../../hooks/UseProducts";
 
 import { DashboardHeader, DashboardMain, Container } from "./styles";
 import { Products } from "../Products";
 
-// eslint-disable-next-line
-import notFound from "../../assets/not-found.jpg";
-
-interface ProductFormatted extends Product {
-    priceFormatted: string;
-    promotionPriceFormatted: string;
-}
-
 export function Dashboard() {
     const { products, productsOnSale } = useProducts();
-    const [productsOnSaleDTO, setProductsOnSaleDTO] = useState<ProductFormatted[]>([]);
-    const [productsDTO, setProductsDTO] = useState<ProductFormatted[]>([]);
-
-    useEffect(() => {
-        const newProductOnSale = productsOnSale.map(product => ({
-            ...product,
-            priceFormatted: formatPrice(product.preco),
-            promotionPriceFormatted: formatPrice(product.preco - (product.preco * (Number(product.desconto) / 100)))
-        }));
-
-        const newProduct = products.map(product => ({
-            ...product,
-            priceFormatted: formatPrice(product.preco),
-            promotionPriceFormatted: formatPrice(product.preco - (product.preco * (Number(product.desconto) / 100)))
-        }));
-        setProductsOnSaleDTO(newProductOnSale);
-        setProductsDTO(newProduct);
-        // eslint-disable-next-line
-    }, []);
 
     return (
         <Container>
             <DashboardHeader>
-                {productsOnSaleDTO.map(product => (
+                {productsOnSale.map(product => (
                     <ProductHeader
                         key={product.id}
                         name={product.nome}
-                        // image={notFound}
+                        image={""}
                         price={product.priceFormatted}
                         promotionPrice={product.promotionPriceFormatted}
                         promotion={Number(product.desconto)}
@@ -56,19 +25,7 @@ export function Dashboard() {
             <h2>BEST SELLER</h2>
 
             <DashboardMain>
-                {productsDTO.map(product => (
-                    <Products
-                        key={product.id}
-                        name={product.nome}
-                        // image={notFound}
-                        promotionPrice={product.promotionPriceFormatted}
-                        price={product.priceFormatted}
-                    />
-                ))}
-            </DashboardMain>
-
-            {/* <DashboardMain>
-                {ProductsDTO.map(product => (
+                {products.map(product => (
                     <Products
                         key={product.id}
                         name={product.nome}
@@ -77,7 +34,7 @@ export function Dashboard() {
                         price={product.priceFormatted}
                     />
                 ))}
-            </DashboardMain> */}
+            </DashboardMain>
         </Container>
     );
 }
--- frontend/src/hooks/UseProducts.tsx
+++ frontend/src/hooks/UseProducts.tsx
@@ -7,27 +7,51 @@ import { Product } from "../types";
 // import produtoHeader3 from "../assets/tenis2.png";
 
 import api from "../services/api";
+import { formatPrice } from "../util/format";
 
 interface ProductsProviderProps {
     children: ReactNode;
 }
 
 interface ProductsContextData {
-    products: Product[];
-    productsOnSale: Product[];
+    products: ProductFormatted[];
+    productsOnSale: ProductFormatted[];
+}
+
+interface ProductFormatted extends Product {
+    priceFormatted: string;
+    promotionPriceFormatted: string;
 }
 
 const ProductsContext = createContext<ProductsContextData>({} as ProductsContextData);
 
 export function ProductsProvider({ children }: ProductsProviderProps) {
-    const [productsOnSale, setProductsOnSale] = useState<Product[]>([]);
+    const [productsOnSale, setProductsOnSale] = useState<ProductFormatted[]>([]);
 
-    const [products, setProducts] = useState<Product[]>([]);
+    const [products, setProducts] = useState<ProductFormatted[]>([]);
 
     useEffect(() => {
-        api.get("/produtos/promocao").then(response => setProductsOnSale(response.data));
-
-        api.get("/produtos/categorizado").then(response => setProducts(response.data));
+        async function loadProducts() {
+            const response = await api.get<ProductFormatted[]>('/produtos/categorizado');
+            const responseOnSale = await api.get<ProductFormatted[]>('/produtos/promocao');
+      
+            const data = response.data.map(product => ({
+              ...product,
+              priceFormatted: formatPrice(product.preco),
+              promotionPriceFormatted: formatPrice(product.preco - (product.preco * (Number(product.desconto) / 100)))
+            }));            
+            
+            const dataOnSale = responseOnSale.data.map(product => ({
+              ...product,
+              priceFormatted: formatPrice(product.preco),
+              promotionPriceFormatted: formatPrice(product.preco - (product.preco * (Number(product.desconto) / 100)))
+            }));
+      
+            setProducts(data);
+            setProductsOnSale(dataOnSale);
+          }
+      
+          loadProducts();
     }, []);
 
     return (
@@ -40,10 +64,3 @@ export function ProductsProvider({ children }: ProductsProviderProps) {
 export function useProducts(): ProductsContextData {
     return useContext(ProductsContext);
 }
-
-        // {id: Math.random(), nome: "FS - QUILTED MAXI CROSS BAG", url: produtoHeader1, preco: 534.33, desconto: 50}, //promotionPrice: 299.43
-        // {id: Math.random(), nome: "FS - QUILTED MAXI CROSS BAG", url: produtoHeader2, preco: 534.33, desconto: 299.43},
-        // {id: Math.random(), nome: "FS - QUILTED MAXI CROSS BAG", url: produtoHeader3, preco: 534.33, desconto: 299.43},
-        // {id: Math.random(), nome: "FS - QUILTED MAXI CROSS BAG", url: produtoHeader1, preco: 534.33, desconto: 299.43},
-        // {id: Math.random(), nome: "FS - QUILTED MAXI CROSS BAG", url: produtoHeader2, preco: 534.33, desconto: 299.43},
-        // {id: Math.random(), nome: "FS - QUILTED MAXI CROSS BAG", url: produtoHeader3, preco: 534.33, desconto: 299.43},
