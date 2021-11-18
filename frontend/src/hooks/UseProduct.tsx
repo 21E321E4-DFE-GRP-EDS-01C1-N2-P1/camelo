5e827b0dd7b5c1bc84cb156eca0ff4db918b3187
carlos-henrique-dos-santos <carlos.hrq.rodrigues@gmail.com>
2021-11-17 22:59:10 -0300

    adiciona context para processar carrinho

--- frontend/src/hooks/UseProduct.tsx
+++ frontend/src/hooks/UseProduct.tsx
@@ -17,7 +17,7 @@ interface Category {
 }
 
 interface ProductContextData {
-    getDetailProductById:(id: number) => Promise<Product>;
+    addProductInLocalStorage:(id: number) => Promise<void>;
 }
 
 interface ProductProviderProps {
@@ -28,22 +28,13 @@ export const ProductContext = createContext<ProductContextData>({} as ProductCon
 
 export function ProductProvider({ children }: ProductProviderProps) {
     
-    async function getDetailProductById(id: number): Promise<Product> {
+    async function addProductInLocalStorage(id: number): Promise<void> {
         
-
-        return {
-            categorias: undefined,
-            desconto: undefined,
-            id: undefined,
-            nome: undefined,
-            preco: undefined,
-            url: undefined
-        }
     }
 
     return (
         <ProductContext.Provider value={{
-            getDetailProductById,
+            addProductInLocalStorage,
         }}>
             {children}
         </ProductContext.Provider>
