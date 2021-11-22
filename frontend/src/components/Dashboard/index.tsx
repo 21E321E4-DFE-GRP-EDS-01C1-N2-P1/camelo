2802c605b8b04841906feb052a109962a1dbdf58
carlos-henrique-dos-santos <carlos.hrq.rodrigues@gmail.com>
2021-11-21 22:42:08 -0300

    adiciona correcoes

--- frontend/src/components/Dashboard/index.tsx
+++ frontend/src/components/Dashboard/index.tsx
@@ -1,9 +1,27 @@
+import { Link } from "react-router-dom";
+
 import { ProductHeader } from "../ProductHeader";
 import { useProducts } from "../../hooks/UseProducts";
-
-import { DashboardHeader, DashboardMain, Container } from "./styles";
 import { Products } from "../Products";
-import { Link } from "react-router-dom";
+
+import price  from '../../assets/price.svg';
+import shipping  from '../../assets/shipping.svg';
+import support  from '../../assets/support.svg';
+import logo from '../../assets/logo.svg';
+import fb from '../../assets/facebook.svg';
+import tt from '../../assets/twitter.svg';
+
+import { 
+  DashboardHeader, 
+  DashboardMain, 
+  Container, 
+  PlataformInfo,
+  PlataformInfoItem,
+  Footer,
+  FooterEmpresa,
+  FooterSocial,
+  FooterContato
+} from "./styles";
 
 export function Dashboard() {
   const { products, productsOnSale } = useProducts();
@@ -39,6 +57,54 @@ export function Dashboard() {
           </Link>
         ))}
       </DashboardMain>
+
+      <PlataformInfo>
+        <PlataformInfoItem>
+          <img src={shipping} alt="free_shipping" />
+          <h3>FREE SHIPPING</h3>
+          <label>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</label>
+        </PlataformInfoItem>
+
+        <PlataformInfoItem>
+          <img src={price} alt="free_shipping" />
+          <h3>100% REFUND</h3>
+          <label>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</label>
+        </PlataformInfoItem>
+
+        <PlataformInfoItem>
+          <img src={support} alt="free_shipping" />
+          <h3>SUPPORT 24/7</h3>
+          <label>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</label>
+        </PlataformInfoItem>        
+      </PlataformInfo>
+
+      <Footer>
+
+        <FooterEmpresa>
+          <div>
+            <img src={logo} alt="logo aplicacao" />
+            <h4>Camelo</h4>
+          </div>
+          <label>Lorem ipsum dolor sit amet consectetur adipisicing elit.</label>
+        </FooterEmpresa>
+
+        <FooterSocial>
+          <h4>Follow Us</h4>
+          <label>Lorem ipsum dolor sit amet consectetur adipisicing elit.</label>
+
+          <div>
+            <img src={fb} alt="facebook aplicação" />
+            <img src={tt} alt="twitter aplicação" />
+          </div>
+        </FooterSocial>
+
+        <FooterContato>
+          <h4>Contact Us</h4>
+          <label>Lorem ipsum dolor sit amet consectetur adipisicing elit.</label>
+        </FooterContato>
+
+      </Footer>
     </Container>
+
   );
 }
--- frontend/src/components/Dashboard/styles.ts
+++ frontend/src/components/Dashboard/styles.ts
@@ -1,10 +1,16 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;  
+  align-items: center;
+
   h2 {
     text-align: center;
     margin: 2rem;
   }
+  text-align: center;
+
 `;
 
 export const DashboardMain = styled.div`
@@ -23,4 +29,79 @@ export const DashboardHeader = styled.div`
   @media(min-width: 720px) {
     margin-top: -8rem;
   }
+`;
+
+export const PlataformInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-top: 50px;
+  margin-bottom: 50px;
+
+  img {
+    width: 50px;
+    height: 50px;
+  }
+
+`;
+
+export const PlataformInfoItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 250px;
+`;
+
+export const Footer = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding-top: 100px;
+  align-content: center;
+  align-self: center;
+  
+  width: 100%;
+  height: 500px;
+  background: var(--blue-light);
+`;
+
+export const FooterEmpresa = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 200px;
+  align-items: center;
+
+  label {
+    text-align: center;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+
+    img {
+      width: 50px;
+      height: 50px;
+    }
+  }
+`;
+
+export const FooterSocial = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 250px;
+
+  div {
+    display: flex;
+
+    img {
+      padding: 0 20px;
+    }
+  }
+`;
+
+export const FooterContato = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 250px;
 `;
